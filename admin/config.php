3eea0f88cad94c29045227bb1a9efe84f44012d0
nguyen bao ngoc <grace.nn1998@gmail.com>
2021-10-14 22:36:35 +0700

    first commit: vietnamese enabled

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/ProgrammingSoftware/XAMPP/apps/ecom/admin/');
+define('DIR_SYSTEM', 'C:/ProgrammingSoftware/XAMPP/apps/ecom/system/');
+define('DIR_IMAGE', 'C:/ProgrammingSoftware/XAMPP/apps/ecom/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/ProgrammingSoftware/XAMPP/apps/ecom/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ecom_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
